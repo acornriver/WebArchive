f07aa43ee03a7813f73a31590208c844bc9ec446
acornriver <kmicky0806@gmail.com>
2025-11-23 15:21:50 +0900

    Remove basePath for custom domain

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/WebArchive' : '',
-  assetPrefix: isProd ? '/WebArchive' : '',
   trailingSlash: true,
 };
 
